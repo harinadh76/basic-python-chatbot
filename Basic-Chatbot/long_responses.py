3b57ef4d94286182521cc5dfb86f87ce500fba5b
Premkumarnaidu Tadi <tannaharinadh@gmail.com>
2023-02-12 17:07:26 +0530

    first commit

---
+++ Basic-Chatbot/long_responses.py
@@ -0,0 +1,5 @@
+import random
+
+def unknown():
+    random_response = ['Could YOu please re-phrase that', '...', 'Sounds about right', 'What does that mean'][random.randrange(4)]
+    return random_response
